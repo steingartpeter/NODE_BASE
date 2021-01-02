b9245344805f220992b1c8802c2254c589dae9ab
steingartpeter <steingart.p2@gmail.com>
2021-01-02 17:43:47 +0100

    UPDT_20210102_003 PAKS2

--- NODE001/app.js
+++ NODE001/app.js
@@ -23,34 +23,41 @@ const server = http.createServer((req, res) => {
     res.write("</body>");
     res.write("</html>");
     return res.end();
-  } else if (url === "/message" && method === "POST") {
+  }
+  if (url === "/message" && method === "POST") {
     // parse the request data:
-    console.log("PARSING LOGIC HERE");
     const body = [];
     req.on("data", (dataChunk) => {
       console.log(dataChunk);
       body.push(dataChunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
-      console.log("parsedBody: " + parsedBody);
+      //console.log("parsedBody: " + parsedBody);
       const msg = parsedBody.split("=")[1];
-      fs.writeFileSync("msg.TXT", msg);
+      // Instead of sync write we use async, but in this case (of course) we need
+      // a callabck as well...
+      fs.writeFile("msg.TXT", msg, (err) => {
+        if (err !== null) {
+          console.err("File writing FAILED.");
+          res.writeHead(500, { Location: "/" });
+          return res.end("<h1>HIBA</h1>");
+        }
+        res.writeHead(302, { Location: "/" });
+        return res.end();
+      });
     });
-    res.writeHead(302, { Location: "/" });
-    return res.end();
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head>");
-    res.write('<meta charset="utf-8"/>');
-    res.write("<title>NODE001</title>");
-    res.write("</head>");
-    res.write("<body>");
-    res.write("<h1>NODEjs In WORK :)</h1>");
-    res.write("</body>");
-    res.write("</html>");
   }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head>");
+  res.write('<meta charset="utf-8"/>');
+  res.write("<title>NODE001</title>");
+  res.write("</head>");
+  res.write("<body>");
+  res.write("<h1>NODEjs In WORK :)</h1>");
+  res.write("</body>");
+  res.write("</html>");
 
   res.end();
 });
